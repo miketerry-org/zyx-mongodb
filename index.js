b2ca9f79426b857b25c43c71519f103829f5315a
Mike Terry <support@miketerry.org>
2025-07-04 17:13:41 -0500

    impoper use of capitalization for userModel.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js: // keeno-mongodb
+// index.js: // zyx-mongodb
 
 "use strict";
 
@@ -7,6 +7,6 @@ const createDB = require("./lib/createDB");
 const closeDB = require("./lib/closeDB");
 const createLog = require("./lib/createLog");
 const closeLog = require("./lib/closeLog");
-const UserModel = require("./lib/UserModel");
+const UserModel = require("./lib/userModel");
 
 module.exports = { createDB, closeDB, createLog, closeLog, UserModel };
