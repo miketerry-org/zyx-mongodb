0c4c48ea0d989c894f291d03f275296cef5aba2d
Mike Terry <support@miketerry.org>
2025-07-20 13:54:04 -0500

    exported MongooseModel and removed UserModel

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const createDB = require("./lib/createDB");
 const closeDB = require("./lib/closeDB");
 const createLog = require("./lib/createLog");
 const closeLog = require("./lib/closeLog");
-const UserModel = require("./lib/userModel");
+const MongooseModel = require("./lib/mongooseModel");
 
-module.exports = { createDB, closeDB, createLog, closeLog, UserModel };
+module.exports = { createDB, closeDB, createLog, closeLog, MongooseModel };
